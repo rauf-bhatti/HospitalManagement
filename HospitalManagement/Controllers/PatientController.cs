da029e16b9fe536124e71266e6710b8f3c6ade0c
rauf-pk <rauf.bhatti.github@gmail.com>
2022-05-28 13:23:54 +0500

    Add Patient Controller Class

---
+++ HospitalManagement/Controllers/PatientController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagement.Models;
+using HospitalManagement.Database;
+
+namespace HospitalManagement.Controllers
+{
+    class PatientController
+    {   
+        private string QuerizePatientEntry(Patient newPatient)
+        {
+            string returnQuery = "INSERT INTO Patients_Table (FirstName, LastName, Age, Address) VALUES (";
+
+            return returnQuery;
+        }
+
+        public void AddPatientEntry(Patient newPatient)
+        {
+
+        }
+    }
+}
