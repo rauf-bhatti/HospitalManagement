778dd35a70bb3e44f3cf9ae71e5adcd2b637d9d5
Yousaf <yousafqadri78@gmail.com>
2022-05-31 00:56:54 +0500

    Inventory Catergory Enumeration added

---
+++ HospitalManagement/Models/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Models
+{
+    class Inventory
+    {
+        int id;
+        int Title;
+        int Quantity;
+
+        public Inventory(int id, int Title, int Quantity)
+        {
+            this.id = id;
+            this.Title = Title;
+            this.Quantity = Quantity;
+
+        }
+
+    }
+}
---
+++ HospitalManagement/Models/InventoryCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Models
+{
+    enum InventoryCategory
+    {
+        DENTALINSTRUEMENTS,
+        SURGICALINSTRUEMENTS,
+        WHEELCHAIR,
+        PATIENTBED,
+        ICUINSTRUEMENTS,
+        ICUBED,
+        OPERATIONTHEATORMACHINE,
+        STRETCHER,
+        EMERGENCYWARDINSTRUEMENTS
+
+
+    }
+}
