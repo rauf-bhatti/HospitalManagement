49a1f3ecdc84753c04202dde38be31f5e0fe57cf
Yousaf <yousafqadri78@gmail.com>
2022-05-31 20:56:36 +0500

    Staff Enumeration Added. Combo Box also added in Staff Entry.

---
+++ HospitalManagement/Models/StaffCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Models
+{
+    public enum StaffCategory
+    {
+        JANITOR,
+        RECEPTIONIST,
+        MANAGER,
+        NURSE,
+        WARD_BOY,
+        SECURITY_GUARD,
+        ADMIN
+
+
+
+    }
+}
