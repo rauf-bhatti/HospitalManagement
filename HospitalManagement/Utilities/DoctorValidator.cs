bed35ef51377966450a1bf972ed2786065662215
rauf-pk <rauf.bhatti.github@gmail.com>
2022-05-31 17:08:30 +0500

    Inventory UI's Added

---
+++ HospitalManagement/Utilities/DoctorValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Utilities
+{
+    class DoctorValidator : IValidator
+    {
+        public bool ValidateData(string dataString)
+        {
+            bool flag = dataString.Any(char.IsDigit);
+
+            return !flag; //Return opposite of flag.
+        }
+
+        public bool ValidateDataLength(string dataString, int lowerBound, int upperBound)
+        {
+            if (dataString.Length <= 0 || dataString.Length < lowerBound) //Empty check is common to all login data that needs to be validated.
+                return false;
+
+            if (dataString.Length >= upperBound)
+                return false;
+
+            return true;
+        }
+    }
+}
