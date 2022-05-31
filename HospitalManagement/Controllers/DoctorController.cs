46aeb42a293bc0bccc33f0c46eedd5b22152118e
rauf-pk <rauf.bhatti.github@gmail.com>
2022-05-31 17:08:30 +0500

    Add Controller and Validator for Doctor Class

---
+++ HospitalManagement/Controllers/DoctorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagement.Utilities;
+using HospitalManagement.Database;
+using HospitalManagement.Models;
+
+namespace HospitalManagement.Controllers
+{
+    class DoctorController
+    {
+        private DoctorValidator doctorValidator = new DoctorValidator();
+        private HospitalManagement.Database.Database database = new HospitalManagement.Database.Database();
+        private readonly string doctor_get_query = "SELECT * FROM Doctor_Table";
+    }
+}
